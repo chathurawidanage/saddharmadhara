80b142d927c3b6ba0cdef8deab2c30e9c5a1af30
Chathura Widanage <chathurawidanage@gmail.com>
2024-07-06 10:52:28 +0530

    update sql

--- admin/sql/retreats.sql
+++ admin/sql/retreats.sql
@@ -3,51 +3,53 @@
  avoids the need to load all options (including historical ones), as the DHIS2 'optionSet'
  endpoint currently lacks filtering by attribute values (e.g., date).
  */
-with retreats_optionset as (
-    select
-        optionsetid
-    from
-        optionset o
-    where
-        o.code = 'Retreat'
+with retreats_optionset as
+(
+select
+	optionsetid
+from
+	optionset o
+where
+	o.code = 'Retreat'
 ),
 retreat_date_attribute as (
-    select
-        uid
-    from
-        attribute a
-    where
-        a.code = 'RetreatDate'
+select
+	uid
+from
+	attribute a
+where
+	a.code = 'RetreatDate'
 )
 select
-    ov.uid as id,
-    ov.code as code,
-    ov.name as name,
-    jsonb_object_agg(
+	ov.uid as id,
+	ov.code as code,
+	ov.name as name,
+	jsonb_object_agg(
         av.key,
-        (value ->> 'value') :: text
-    ) AS attributeValues
-from
-    optionvalue ov
-    JOIN LATERAL jsonb_each(ov.attributevalues) av ON true
-where
-    ov.optionsetid = (
-        select
-            optionsetid
-        from
-            retreats_optionset
-    )
-    and (
-        (
-            ov.attributevalues ->(
-                select
+	(value->>'value')::text
+    ) as attributeValues,
+   CASE
+        WHEN ((ov.attributevalues->(
+                SELECT
                     uid
-                from
+                FROM
                     retreat_date_attribute
-            )
-        ) ->> 'value'
-    ) :: date > CURRENT_DATE + interval '14 days'
-GROUP BY
-    ov.uid,
-    ov.code,
-    ov.name;
+            ))->>'value')::date > CURRENT_DATE - interval '14 days' THEN true
+        ELSE false
+    END AS current
+from
+	optionvalue ov
+join lateral jsonb_each(ov.attributevalues) av on
+	true
+where
+	ov.optionsetid = (
+	select
+		optionsetid
+	from
+		retreats_optionset)
+group by
+	ov.uid,
+	ov.code,
+	ov.name,
+	current
+limit 100
