1810e8887a8197ad2168a7a014c84c350017aff9
Chathura Widanage <chathurawidanage@gmail.com>
2024-07-31 14:23:11 +0530

    data import script

--- utils/attendance_backlog.js
+++ utils/attendance_backlog.js
@@ -39,8 +39,8 @@ const findYogi = async (identification) => {
     }
 };
 
-const readFile = async (fileName) => {
-    console.log("Processing", fileName);
+const checkState = async (fileName) => {
+    console.log("Checking state of ", fileName);
     const lineReader = readline.createInterface({
         input: fs.createReadStream(`${rootPath}/${fileName}`)
     });
@@ -62,15 +62,89 @@ const readFile = async (fileName) => {
 
     if (missing === 0) {
         console.log(fileName, "is safe to import!!!");
+        return true;
     }
-    console.log("");
+    return false;
 };
 
-fs.readdir(rootPath, async (err, files) => {
-    for (let index = 0; index < files.length; index++) {
-        await readFile(files[index]);
+const markYogiAttended = async (teiId, retreatOptionCode, orgUnitId, retreatDate) => {
+    let response = await fetch("https://manager.srisambuddhamission.org/api/events", {
+        method: "POST",
+        body: JSON.stringify({
+            "orgUnit": orgUnitId,
+            "program": "KdYt2OP9VjD",
+            "programStage": "NYxnKQd6goA",
+            "status": "ACTIVE",
+            "trackedEntityInstance": teiId,
+            "eventDate": retreatDate,
+            "dataValues": [
+                {
+                    "dataElement": "rYqV3VQu7LS",
+                    "value": retreatOptionCode,
+                    "providedElsewhere": false
+                }, {
+                    "dataElement": "CzwVwJ30hTj",
+                    "value": "attended",
+                    "providedElsewhere": false
+                }
+            ]
+        }),
+        ...requestOptions
+    });
+
+    let json = await response.json();
+
+    if (json.status === "OK") {
+        console.log(teiId, "DONE");
+    } else {
+        console.log(teiId, "FAILED");
     }
-});
+};
+
+const markFileAttendance = async (fileName, retreatOptionCode, orgUnitId, retreatDate) => {
+
+    if (!await checkState(fileName)) {
+        console.log("Skipping", fileName, "due to invalid state");
+        return;
+    }
+
+    console.log("Processing", fileName);
+    const lineReader = readline.createInterface({
+        input: fs.createReadStream(`${rootPath}/${fileName}`)
+    });
+
+    let missing = 0;
+
+    let checks = [];
+
+    for await (const idNumber of lineReader) {
+        let teiId = await findYogi(idNumber);
+        await markYogiAttended(teiId, retreatOptionCode, orgUnitId, retreatDate);
+    }
+};
+
+// fs.readdir(rootPath, async (err, files) => {
+//     for (let index = 0; index < files.length; index++) {
+//         await checkState(files[index]);
+//     }
+// });
+
+// markFileAttendance("../junk/5GS1.txt", "Nov 9 2023 Attanagalle", "VfExRWiI0ZJ", "2023-11-09");
+// markFileAttendance("../junk/5GS2.txt", "Nov 17 2023 Attanagalle", "VfExRWiI0ZJ", "2023-11-17");
+// markFileAttendance("../junk/5GS3.txt", "Nov 25 2023 Attanagalle", "VfExRWiI0ZJ", "2023-11-25");
+// markFileAttendance("../junk/5GS4.txt", "Dec 15 2023 Attanagalle", "VfExRWiI0ZJ", "2023-12-15");
+// markFileAttendance("../junk/5GS5.txt", "Dec 22 2023 Attanagalle", "VfExRWiI0ZJ", "2023-12-22");
+// markFileAttendance("../junk/5GS6.txt", "Jan 3 2024 Attanagalle", "VfExRWiI0ZJ", "2024-01-03");
+// markFileAttendance("../junk/5GS7.txt", "Jan 11 2024 Attanagalle", "VfExRWiI0ZJ", "2024-01-11");
+// markFileAttendance("../junk/5GS8.txt", "Mar 28 2024 Attanagalle", "VfExRWiI0ZJ", "2024-03-28");
+// markFileAttendance("../junk/5GS9.txt", "Apr 5 2024 Colombo", "DDfFvmtZQeb", "2024-04-05");
+// markFileAttendance("../junk/10GE1.txt", "Mar 15 2024 Attanagalle", "VfExRWiI0ZJ", "2024-03-15");
+// markFileAttendance("../junk/10GS1.txt", "Oct 27 2023 Attanagalle", "VfExRWiI0ZJ", "2023-10-27");
+// markFileAttendance("../junk/10GS2.txt", "Dec 2 2023 Attanagalle", "VfExRWiI0ZJ", "2023-12-02");
+
+// markYogiAttended("MR6scBzLbgI", "Dec 15 2023 Attanagalle", "VfExRWiI0ZJ", "2024-01-01");
+
+
 
 
 
