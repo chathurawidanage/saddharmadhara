a0bf8853921d51ae97e50ccc27bde607a475713c
chathura <chathurawidanage@gmail.com>
2025-06-26 13:18:37 +0930

    choices change

--- application/src/pages/retreats.js
+++ application/src/pages/retreats.js
@@ -19,7 +19,14 @@ const retreatsPage = {
         [SINHALA_LOCALE]:
           "ඔබ සම්බන්ධ වීමට බලාපොරොත්තු වන වැඩසටහන/වැඩසටහන් තෝරන්න.",
       },
+      description: {
+        [ENGLISH_LOCALE]:
+          "You may apply for up to four programs that are most convenient for you; however, you will be offered only one opportunity. Once the committee assigns a date based on your preferences, if you choose not to accept that date, your opportunity for this year will be forfeited.",
+        [SINHALA_LOCALE]:
+          "මෙහිදී ඔබට වඩාත් පහසු වැඩසටහන් හතරක් සඳහා අයදුම් කළ හැකි නමුදු, ඒ අතරින් ඔබට හිමි වන්නේ එක් අවස්ථාවක් පමණි. ඔබගේ කැමැත්ත පරිදි ඉල්ලුම් කරන එම දිනය කමිටුව විසින් ලබා දුන් පසුව, ඔබ විසින් එම දිනය පිළිනොගන්නේ නම් මෙම වර්ෂයට හිමි අවස්ථාව ගිලී යනු ඇත.",
+      },
       colCount: 1,
+      maxSelectedChoices: 4,
       itemComponent: RETREAT_CHECKBOX_ITEM_NAME,
       choices: [],
     },
