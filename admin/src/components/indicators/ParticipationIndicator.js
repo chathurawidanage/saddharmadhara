c4ed67b021ec4a655c7574c3a3b1815c4dfbbd4b
chathura <chathurawidanage@gmail.com>
2025-08-16 15:57:17 +0530

    bug fix

--- admin/src/components/indicators/ParticipationIndicator.js
+++ admin/src/components/indicators/ParticipationIndicator.js
@@ -1,44 +1,52 @@
-import {
-    Tooltip
-} from "@dhis2/ui";
+import { Tooltip } from "@dhis2/ui";
 import { observer } from "mobx-react";
 import "./ApplicationIndicator.css";
 import COMMENT from "./img/comment.png";
 
 const ParticipationIndicator = observer(({ trackedEntity, store }) => {
-
-    return (
-        <div className="yogi-applications">
-            {Object.keys(trackedEntity.participation)
-                .map(retreatId => {
-                    let retreat = store.metadata.retreatsMapWithCodeKey[retreatId];
-                    let p = trackedEntity.participation[retreatId];
-                    let hasSpecialComment = p.specialComment && p.specialComment.trim().length > 0;
-                    return (
-                      <Tooltip content={<>
-                        <p>
-                          {retreat.name}
-                        </p>
-                        {hasSpecialComment && <p>{p.specialComment}</p>}
-                      </>} key={retreat.code + p.attendance}>
-                            <div className="yogi-application">
-                                <div className="yogi-application-retreat">
-                                    {retreat.retreatCode || "UNKW"}
-                                </div>
-                                <div className={`yogi-application-state participation-state-${p.attendance}`}>
-                                    {p.attendance || "Pending"}
-                                </div>
-                              {hasSpecialComment &&
-                                <div className="yogi-application-special-comment">
-                                  <img src={COMMENT} width={15} alt="comment"/>
-                                </div>
-                              }
-                            </div>
-                        </Tooltip>
-                    );
-                })}
-        </div>
-    )
+  console.log(JSON.stringify(trackedEntity.participation));
+  return (
+    <div className="yogi-applications">
+      {Object.keys(trackedEntity.participation)
+        .filter((retreatId) => retreatId !== undefined)
+        .map((retreatId) => {
+          let retreat = store.metadata.retreatsMapWithCodeKey[retreatId];
+          if (!retreat) {
+            return null;
+          }
+          let p = trackedEntity.participation[retreatId];
+          let hasSpecialComment =
+            p.specialComment && p.specialComment.trim().length > 0;
+          return (
+            <Tooltip
+              content={
+                <>
+                  <p>{retreat.name}</p>
+                  {hasSpecialComment && <p>{p.specialComment}</p>}
+                </>
+              }
+              key={retreat.code + p.attendance}
+            >
+              <div className="yogi-application">
+                <div className="yogi-application-retreat">
+                  {retreat.retreatCode || "UNKW"}
+                </div>
+                <div
+                  className={`yogi-application-state participation-state-${p.attendance}`}
+                >
+                  {p.attendance || "Pending"}
+                </div>
+                {hasSpecialComment && (
+                  <div className="yogi-application-special-comment">
+                    <img src={COMMENT} width={15} alt="comment" />
+                  </div>
+                )}
+              </div>
+            </Tooltip>
+          );
+        })}
+    </div>
+  );
 });
 
 export default ParticipationIndicator;
